aa57238157417fa9ab49628e24a93feec64114c8
khafagy <98766643+ahmedkhafagy800@users.noreply.github.com>
2024-12-04 23:34:41 +0200

    login logic

---
+++ app/src/main/java/com/mina/yasser/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.mina.yasser;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void login(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void signUP(View view) {
+        Intent intent = new Intent(this, RegActivity.class);
+        startActivity(intent);
+    }
+}
